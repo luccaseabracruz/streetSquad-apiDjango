7a9ba4640d25d156ca043dc96c4cdf1f989bb45c
unknown <wellington.eliteinfo@gmail.com>
2023-06-30 18:03:30 -0300

    feat:product model view url e serializer

--- products/views.py
+++ products/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from .serializers import ProductSerializer
+from .models import Product
+from rest_framework import generics
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import (
+    IsAdminUser,
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly
+)
 
-# Create your views here.
+
+class ProductView(generics.ListAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class CreateProductView(generics.CreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        return serializer.save(user=self.request.user)
+
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminUser, IsAuthenticated]
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
