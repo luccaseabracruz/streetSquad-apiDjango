2349070f9c737f3f80cda8eb64c89800519eceff
unknown <wellington.eliteinfo@gmail.com>
2023-07-04 13:59:18 -0300

    feat:update rules in products

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,5 @@ from products import views
 
 urlpatterns = [
     path("products/", views.ProductView.as_view()),
-    path("products/", views.CreateProductView.as_view()),
-    path("products/<int:product_id>", views.ProductDetailView.as_view())
+    path("products/<int:pk>/", views.ProductDetailView.as_view())
 ]
